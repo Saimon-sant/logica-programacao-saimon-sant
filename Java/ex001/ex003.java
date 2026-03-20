bd17dcdcd64b03a7a05504463ec9c28713c5eb42
Saimon-sant <saimonbruno9@gmail.com>
2026-03-20 10:19:25 -0300

    correção do exercício 3 da aula 1 de Java

--- Java/ex001/ex003.java
+++ Java/ex001/ex003.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class ex003 {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Quanto é 2 + 2?");
-            Scanner input = new Scanner(System.in);
-            System.out.print("Digite a resposta: ");
-            String resposta = input.nextLine();
+        System.out.print("Digite a resposta: ");
+
+        int resposta = input.nextInt();
+
         System.out.println("A resposta é: " + resposta);
+
+        input.close();
     }
     
 }
